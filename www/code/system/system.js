b3a509528f17f8b9bfb165d08d96a86c7d3a3ebb
华超 <huachao1001@qq.com>
2026-07-16 11:54:49 +0800

    修改系统命令为异步demo

--- www/code/system/system.js
+++ www/code/system/system.js
@@ -3,5 +3,6 @@ console.log("=== 执行系统命令 ===\n");
 var cmd = "dir c:\\";
 console.log("执行命令: " + cmd);
 
-var output = await W.system(cmd);
-console.log("命令输出: " + output);
+W.system(cmd).then(function (output) {
+    console.log("命令输出: " + output);
+});
